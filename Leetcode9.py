57563270115c8863402c05b32a922285f02580ef
abmac <aleksbrsc@gmail.com>
2023-05-15 16:38:23 -0400

    added new solutions and tests to 9

--- Leetcode9.py
+++ Leetcode9.py
@@ -17,8 +17,60 @@ class Solution:
         else: 
             return False
         
+    # Follow up, can you do it without converting an int to a string?
+    # (supposedly at least a medium problem)
+    def isPalindromeFollowUp(self, x: int) -> bool:
+        # negative numbers are always false
+        if x < 0: return False
+
+        # finding the digits place
+        div = 1
+        while x >= 10 * div:
+            div *= 10
+
+        while x:
+            right = x % 10
+            left = x // div
+
+            if left != right: return False
+            x = (x % div) // 10
+            div = div / 100
+        return True
+
+    def isPalindromeSimpler(self, x: int) -> bool:
+        if x < 0: return False
+
+        c = x
+        b = 0
+        while c:
+            b = b * 10 + c % 10
+            print(b)
+            c //= 10
+            print(c)
+        return b == x
+
 solution = Solution()
+
+# regular tests
+print("\nregular")
+
 print(solution.isPalindrome(123))
 print(solution.isPalindrome(121))
 print(solution.isPalindrome(-1))
 print(solution.isPalindrome(1))
+
+# follow up tests
+print("\nfollow up")
+
+print(solution.isPalindromeFollowUp(123))
+print(solution.isPalindromeFollowUp(121))
+print(solution.isPalindromeFollowUp(-1))
+print(solution.isPalindromeFollowUp(1))
+
+# simpler tests
+print("\nsimpler")
+
+print(solution.isPalindromeSimpler(123))
+print(solution.isPalindromeSimpler(121))
+print(solution.isPalindromeSimpler(-1))
+print(solution.isPalindromeSimpler(1))
