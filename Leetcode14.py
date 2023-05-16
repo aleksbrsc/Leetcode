d2e7e6824d1819b9950abd48df737f24acf066ce
abmac <aleksbrsc@gmail.com>
2023-05-16 00:11:08 -0400

    added comments to 14

--- Leetcode14.py
+++ Leetcode14.py
@@ -2,19 +2,26 @@
 
 class Solution:
     def longestCommonPrefix(self, strs: list[str]) -> str:
-        shortest_word_length = len(min(strs, key=len))
-        current = []
-        prefix = ""
+        shortest_word_length = len(min(strs, key=len)) # range is always the length of shortest word 
+        current = [] # list for holding the characters of the current index in the range
+        prefix = "" # the longest common prefix initially
 
-        for index in range(shortest_word_length): # iterates through all the words
+        for index in range(shortest_word_length): # iterates through range of the length of shortest word
             current = []
-            for word in strs:
-                current.append(word[index])
-                if len(current) == len(strs) and all(letter == current[0] for letter in current):
-                    prefix += current[0]
-                    current = []
+            for word in strs: # iterates through the words in the list
+                current.append(word[index]) # add char to the current list
+                if len(current) == len(strs) and all(letter == current[0] for letter in current): # if list is full with same char
+                    prefix += current[0] # adds the common char to prefix string
+                    current = [] # resets the current list
+            if len(current) == len(strs): # if list is full but not every char is the same
+                return prefix # common prefix ends and is returned
         return prefix
 
-strs = ["flow","flower","flight"]
+# test cases
+test = ["flow","flower","flight"] 
+test2 = ["dog", "racecar", "car"]
+test3 = ["car", "cir"]
 solution = Solution()
-print(solution.longestCommonPrefix(strs))
+print(solution.longestCommonPrefix(test))
+print(solution.longestCommonPrefix(test2))
+print(solution.longestCommonPrefix(test3))
