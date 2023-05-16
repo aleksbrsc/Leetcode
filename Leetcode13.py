32b3857aa03b5621aed4dacb23cc02bdacef079b
abmac <aleksbrsc@gmail.com>
2023-05-15 22:27:04 -0400

    13 done

---
+++ Leetcode13.py
@@ -0,0 +1,21 @@
+# Leetcode 13: Roman to Integer (easy)
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        romans = { "I" : 1, "V" : 5, "X" : 10, 
+                  "L" : 50, "C" : 100, "D" : 500, "M" : 1000}
+        
+        result = 0
+
+        for i in range(len(s)):
+            if i + 1 < len(s) and romans[s[i]] < romans[s[i + 1]]:
+                result -= romans[s[i]]
+            else:
+                result += romans[s[i]]
+
+        return result
+        
+
+solution = Solution()
+
+print(solution.romanToInt("IX"))
