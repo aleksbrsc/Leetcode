15f5acbb457a6cb4599d47573152464a73bc2a40
abmac <aleksbrsc@gmail.com>
2023-05-17 11:49:53 -0400

    finished 347

--- Leetcode20.py
+++ Leetcode20.py
@@ -5,12 +5,12 @@ class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
         # hashmap mapping closed brackets to their corresp. open bracket 
-        close_to_open = { ")" : "(",
+        closed_to_open = { ")" : "(",
                         "]" : "[",
                         "}" : "{"} 
         for char in s: # iterate through each character in string
-            if char in close_to_open: # if its a closed bracket
-                if stack and stack[-1] == close_to_open[char]: # if the stack is not empty and its last item is a corresp. open bracket
+            if char in closed_to_open: # if its a closed bracket
+                if stack and stack[-1] == closed_to_open[char]: # if the stack is not empty and its last item is a corresp. open bracket
                     stack.pop() # removes last open bracket from the stack
                 else:
                     return False # returns false if not in the stack or wrong bracket type
---
+++ Leetcode347.py
@@ -0,0 +1,32 @@
+# Leetcode 347: Top K Frequent Elements
+# arrays, hashing
+from collections import defaultdict
+class Solution:
+    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
+        count_map = defaultdict(list) # number : count
+        result = []
+
+        # appending each number and the amount of times it appears to the count map
+        for n in nums:
+            if count_map[n]: # if the value is not null / already exists
+                count_map[n] = count_map[n] + 1 # increment the count value
+            else: 
+                count_map[n] = 1 # set the count to 1
+        
+        # sort hashmap by its values in reverse order, basically its count in descending order
+        # sorted returns a list therefore cast back to defaultdict
+        sorted_count_map = dict(sorted(count_map.items(), key=lambda x:x[1], reverse=True))
+
+        # append the sorted keys to the result list until k amount is reached
+        for i, key in enumerate(sorted_count_map):
+            if i < k:
+                result.append(key)
+            
+        return result
+
+solution = Solution()
+
+leetcodetest = [4,1,-1,2,-1,2,3]
+
+print(solution.topKFrequent([2, 2, -2, 2, 1, 3, 4, 5, 3], 6))
+print(solution.topKFrequent(leetcodetest, 2))
