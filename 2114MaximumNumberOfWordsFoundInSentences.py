f97277942237ca0f22f9b08e4b9dbfb1c1cecd3c
abmac <aleksbrsc@gmail.com>
2023-05-17 17:03:29 -0400

    finished and commented 2114

---
+++ 2114MaximumNumberOfWordsFoundInSentences.py
@@ -0,0 +1,28 @@
+# Leetcode 2114: Maximum number of words found in setence (easy, easiest)
+# string
+
+class Solution(object):
+    def mostWordsFound(self, sentences):
+        """
+        :type sentences: List[str]
+        :rtype: int
+        """
+        word_counts = []
+
+        for s in sentences:
+            words = s.split() # 
+            print(words)
+            print(s)
+            count = 0
+            for word in words:
+                count += 1
+            word_counts.append(count)
+
+        # sort the list in descending order
+        sorted_word_count = sorted(word_counts, reverse=True)
+
+        return sorted_word_count[0] # return the first (highest) value of the sorted list
+
+# test cases
+solution = Solution()
+print(solution.mostWordsFound(["blah blah blah", "blah blah"]))
