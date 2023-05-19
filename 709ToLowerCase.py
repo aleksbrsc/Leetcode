61e02faee041c797368d7938891aecae8ad98dac
abmac <aleksbrsc@gmail.com>
2023-05-18 22:49:17 -0400

    finished 709

---
+++ 709ToLowerCase.py
@@ -0,0 +1,14 @@
+# Leetcode 709: To Lower Case (easy)
+# string
+
+class Solution(object):
+    def toLowerCase(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return s.lower()
+    
+# test cases
+solution = Solution()
+print(solution.toLowerCase("sfasfWasdasd"))
