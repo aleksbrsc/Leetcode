41f19bb24c288824c0e74e57b4c510fc8cecc14f
abmac <aleksbrsc@gmail.com>
2023-05-21 16:36:55 -0400

    fixed a solution in 2418

--- 2418SortThePeople.py
+++ 2418SortThePeople.py
@@ -2,21 +2,21 @@
 # string, hashmap
 from collections import defaultdict
 class Solution(object):
-    # works everywhere else besides leetcode, does leetcode interpreter not like defaultdict?
+    # note: when you want to sort a dictionary in python you want to sort by the keys.
     def sortPeople(self, names, heights):
         """
         :type names: List[str]
         :type heights: List[int]
         :rtype: List[str]
         """
-        people = defaultdict(list)
+        people = {}
 
         for i, n in enumerate(names):
-            people[n] = heights[i]
+            people[heights[i]] = n
         
         ordered = dict(sorted(people.items())[::-1]) # slice notation but can use reverse=True inside sorted method
 
-        return ordered.keys()
+        return ordered.values()
     
     # works on leetcode, zip function quickly maps two lists together
     def sortPeople2(self, names, heights):
@@ -26,6 +26,15 @@ class Solution(object):
         for height in sorted(height_map.keys())[::-1]: 
             ordered.append(height_map[height])
 
+        return ordered
+    
+    def sortPeople3(self, names, heights):
+        ordered = []
+        height_map = dict(zip(heights, names))
+
+        for height in sorted(height_map.keys(), reverse=True):
+            ordered.append(height_map[height])
+        
         return ordered
     
 # test cases
