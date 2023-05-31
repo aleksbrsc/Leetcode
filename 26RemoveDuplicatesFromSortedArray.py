28a0864c8a508898360a073d668b77e0153cbba8
abmac <aleksbrsc@gmail.com>
2023-05-31 00:55:49 -0400

    finished 26

---
+++ 26RemoveDuplicatesFromSortedArray.py
@@ -0,0 +1,18 @@
+# Leetcode 26. Remove Duplicates from Sorted Array (easy, flawed description)
+# array
+
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums = sorted(set(nums))
+        return len(nums)
+
+# test cases
+solution = Solution()
+nums = [1,1,2]
+print(solution.removeDuplicates(nums))
+nums = [0,0,1,1,1,2,2,3,3,4]
+print(solution.removeDuplicates(nums))
