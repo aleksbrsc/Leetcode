4333c03072f51757ecb8036fb1f18b1edb78638e
abmac <aleksbrsc@gmail.com>
2023-08-08 19:25:17 +0300

    2496

---
+++ 2496MaximumValueOfAStringInAnArray.py
@@ -0,0 +1,38 @@
+# Leetcode 2496. Maximum Value of a String in an Array (easy)
+# string, array
+
+class Solution(object):
+    def maximumValue(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: int
+        """
+        digits = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+                    "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+        counts = []
+
+        for string in strs:
+            count = 0
+            contains_letters = False
+            for char in string:
+                if char in letters:
+                    contains_letters = True
+            if contains_letters == True:
+                counts.append(len(string))
+                continue
+            counts.append(int(string))
+
+
+                    
+
+
+        return max(counts)
+
+            
+# test cases
+solution = Solution()
+strs = ["alic3","bob","3","4","00000"]
+print(solution.maximumValue(strs)) # 5
+strs = ["1","01","001","0001"] 
+print(solution.maximumValue(strs)) # 1
