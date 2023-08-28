5fb8427d4a2f87436251d88f854c55a7867f5f4b
abmac <aleksbrsc@gmail.com>
2023-08-28 14:35:56 +0200

    238

---
+++ 238ProductOfArrayExceptSelf.py
@@ -0,0 +1,42 @@
+# Leetcode 238. Product of Array Except Self (easy)
+# array
+
+class Solution(object):
+    def productExceptSelf(self, nums):
+        ans = [1] * len(nums) 
+        
+        prefix, postfix = 1, 1
+        for i in range(len(nums)):
+            ans[i] = prefix
+            prefix *= nums[i]
+        for i in range(len(nums)-1, -1, -1):
+            ans[i] *= postfix
+            postfix *= nums[i]
+
+        return ans
+
+    def productExceptSelf2(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        ans = [1] * len(nums)
+
+        prefix = 1
+        for i in range(len(nums)):
+            ans[i] = prefix
+            prefix *= nums[i]
+
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            ans[i] *= postfix
+            postfix *= nums[i]
+
+        return ans
+
+# test cases
+solution = Solution()
+nums = [1,2,3,4]
+print(solution.productExceptSelf(nums)) # [24,12,8,6]
+nums = [-1,1,0,-3,3]
+print(solution.productExceptSelf(nums)) # [0,0,9,0,0]
