50bb9564cc7c113ee71775590967b509ce65942f
Aleksandar Bursac <bursaca@sheridancollege.ca>
2024-08-03 11:19:59 +0300

    finished 125

--- 125ValidPalindrome.py
+++ 125ValidPalindrome.py
@@ -6,14 +6,24 @@
 
 # Given a string s, return true if it is a palindrome, or false otherwise.
 
-
+import re
 
 class Solution:
     def isPalindrome(self, s):
-        return
+        s = s.replace(" ", "").lower()
+        s = re.sub(r'[^a-zA-Z0-9]','', s) # regex removes all non-alphanumeric chars
+        odd = len(s) % 2
+
+        for i in range(0, (len(s)// 2) + odd):
+            left = s[i]
+            right = s[-1-i]
+            if left != right: return False
+
+        return True
+
 
 solution = Solution()
-print(solution.isPalindrome("A man, a plan, a canal: Panama"))
-print(solution.isPalindrome("race a car"))
-print(solution.isPalindrome(" "))
+print(solution.isPalindrome("A man, a plan, a canal: Panama")) # True
+print(solution.isPalindrome("race a car")) # False
+print(solution.isPalindrome(" ")) # True
     
